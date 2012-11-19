2a5f34f986fc0b3041ca2923fe3e2deb0018aea0
Hank Yates <hank@c4tech.com>
2012-11-18 23:24:49 -0600

    on window load instead

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,14 @@
-require([
-  'static/js/page'
-  ], function(){
-    var pages = new Pages();
-    var page_view = new PageView({collection: pages});
+$(window).on('load', function(){
+  require([
+    'static/js/page'
+    ], function(){
+      var pages = new Pages();
+      var page_view = new PageView({collection: pages});
 
-    pages.fetch({
-      success: function(){
-        page_view.render();
-      }
-    });
+      pages.fetch({
+        success: function(){
+          page_view.render();
+        }
+      });
+  });
 });
