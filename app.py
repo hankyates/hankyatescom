6268bd4ed145e2d880c943b751031db07f3859b5
Hank Yates <hank@c4tech.com>
2013-01-30 03:06:31 -0600

    new blog post + github link

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ def pages():
     },{
       'title': 'What should I use for persistence?',
       'content': '<p>Right now I\'m just hardcoding in a hash to my flask application to generate content. Seems like if I ever get serious about blogging I should break my content posts into some sort of db. Postgres seems like overkill since all I\'ll ever have for content is text and a title and a content. Maybe Redis or MongoDB?</p>'
+    },{
+      'title': 'DataTrail',
+      'content': '<p>An old friend of mine, <a href="http://derekgpoole.wordpress.com/">Derek Poole</a>, and I decided to start being creative again. This time instead of <a href="http://www.myspace.com/notvsintheocean">music</a>, we decided to tell a story with a video game. Thus <a href="https://github.com/hankyates/datarail">DataTrail</a> was born.</p><p>As far as tech is concerned I am using <a href="http://requirejs.org/">Require</a> and <a href="http://backbonejs.org/">Backbone</a>. Originally I was using <a href="http://craftyjs.com/">Crafty</a>, but after a co-worker pointed me towards <a href="http://www.createjs.com/#!/CreateJS">CreateJs</a>, I have been refactoring my code to use it instead. Being a fan of micro frameworks, I like the control of something light and modular like CreateJs. Hopefully our proof of concept demo will be up soon!</p> '
     }
   return make_response(dumps(pages[::-1]))
 
